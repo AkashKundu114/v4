3b49a5c2b4655b8969137c8f4fc0a8ebc009dee3
AkashKundu114 <68686532+AkashKundu114@users.noreply.github.com>
2020-11-29 13:57:40 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,11 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Akash Kundu</h2>;
+  const three = <h3 className="big-heading">I'm a Junior Developer</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I am a school student and occasionally designing websites, applications, and everything in between. I do developer friendly code which is very too easy to understand and customized it as per needs.
     </p>
   );
   const five = (
